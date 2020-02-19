62fdb35d55fb13c938539956aa5e61c28aa45795
Yuriy Cherniy <amidobox@gmail.com>
2020-02-19 23:44:00 +0300

    test: added test and add_items_to_db function  needed for some tests

--- local_shop_cms/mainpage/tests/test_views.py
+++ local_shop_cms/mainpage/tests/test_views.py
@@ -7,6 +7,16 @@ from mainpage.models import ItemOnMainPage
 from showcase.models import Item
 
 
+def add_items_to_db(item_amount):
+    for i in range(item_amount):
+        Item.objects.create(
+            title='test', description='test', image='/test.jpg', price=100
+        )
+        ItemOnMainPage.objects.create(
+            item_on_main_page_id=(i + 2), position=(i + 2)
+        )
+
+
 class TestViews(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -24,7 +34,7 @@ class TestViews(TestCase):
     def setUp(self):
         self.c = Client()
 
-# Status code 403 tests
+# status code 403 tests
     def test_main_page_editor_list_view_status_code_403(self):
         response = self.c.get(reverse('main_page_editor_url'))
         self.assertEquals(response.status_code, 403)
@@ -45,7 +55,7 @@ class TestViews(TestCase):
         )
         self.assertEquals(response.status_code, 403)
 
-# Status code 200 and 302 tests
+# status code 200 and 302 tests
     def test_main_page_item_list_view_status_code_200(self):
         response = self.c.get(reverse('main_page_url'))
         self.assertEquals(response.status_code, 200)
@@ -68,13 +78,7 @@ class TestViews(TestCase):
         self.c.post(
             '/admin/login/', {'username': 'test', 'password': '0000'}
         )
-        for i in range(8):
-            Item.objects.create(
-                title='test', description='test', image='/test.jpg', price=100
-            )
-            ItemOnMainPage.objects.create(
-                item_on_main_page_id=(i + 2), position=(i + 2)
-            )
+        add_items_to_db(8)
         response = self.c.get(reverse('add_item_to_main_page_url'))
         self.assertEquals(response.status_code, 302)
 
@@ -96,7 +100,7 @@ class TestViews(TestCase):
         )
         self.assertEquals(response.status_code, 302)
 
-# TemplateUsed test cases
+# templateUsed test cases
     def test_main_page_item_list_template_used(self):
         response = self.c.get(reverse('main_page_url'))
         self.assertTemplateUsed(response, 'mainpage/main_page_item_list.html')
@@ -106,28 +110,26 @@ class TestViews(TestCase):
             '/admin/login/', {'username': 'test', 'password': '0000'}
         )
         response = self.c.get(reverse('main_page_editor_url'))
-        self.assertTemplateUsed(response, 'mainpage/main_page_item_editor_list.html')
+        self.assertTemplateUsed(
+            response, 'mainpage/main_page_item_editor_list.html')
 
     def test_main_page_editor_create_view_template_used(self):
         self.c.post(
             '/admin/login/', {'username': 'test', 'password': '0000'}
         )
         response = self.c.get(reverse('add_item_to_main_page_url'))
-        self.assertTemplateUsed(response, 'mainpage/item_on_main_page_form.html')
+        self.assertTemplateUsed(
+            response, 'mainpage/item_on_main_page_form.html')
 
     def test_main_page_editor_create_view_redirect_template_used(self):
         self.c.post(
             '/admin/login/', {'username': 'test', 'password': '0000'}
         )
-        for i in range(8):
-            Item.objects.create(
-                title='test', description='test', image='/test.jpg', price=100
-            )
-            ItemOnMainPage.objects.create(
-                item_on_main_page_id=(i + 2), position=(i + 2)
-            )
-        response = self.c.get(reverse('add_item_to_main_page_url'), follow=True)
-        self.assertTemplateUsed(response, 'mainpage/main_page_item_editor_list.html')
+        add_items_to_db(8)
+        response = self.c.get(
+            reverse('add_item_to_main_page_url'), follow=True)
+        self.assertTemplateUsed(
+            response, 'mainpage/main_page_item_editor_list.html')
 
     def test_main_page_editor_update_view_template_used(self):
         self.c.post(
@@ -136,7 +138,8 @@ class TestViews(TestCase):
         response = self.c.get(
             reverse('update_item_on_main_page_url', args=[1])
         )
-        self.assertTemplateUsed(response, 'mainpage/item_on_main_page_form.html')
+        self.assertTemplateUsed(
+            response, 'mainpage/item_on_main_page_form.html')
 
     def test_main_page_editor_delete_view_template_used(self):
         self.c.post(
@@ -145,5 +148,14 @@ class TestViews(TestCase):
         response = self.c.delete(
             reverse('delete_item_from_main_page_url', args=[1]), follow=True
         )
-        self.assertTemplateUsed(response, 'mainpage/main_page_item_editor_list.html')
+        self.assertTemplateUsed(
+            response, 'mainpage/main_page_item_editor_list.html')
 
+# some tests
+    def test_item_reposition_after_deleting(self):
+        self.c.post(
+            '/admin/login/', {'username': 'test', 'password': '0000'}
+        )
+        add_items_to_db(2)
+        self.c.delete(reverse('delete_item_from_main_page_url', args=[1]))
+        self.assertEquals(ItemOnMainPage.objects.get(pk=3).position, 2)
