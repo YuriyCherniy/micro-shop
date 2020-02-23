bf91fce346813ff0c04a753d2aaea96936c36f61
Yuriy Cherniy <amidobox@gmail.com>
2020-02-23 22:10:00 +0300

    tests: added tests for ItemModelForm in showcase app

--- local_shop_cms/showcase/tests/test_forms.py
+++ local_shop_cms/showcase/tests/test_forms.py
@@ -1,23 +1,55 @@
+from io import BytesIO
+
+from PIL import Image
+
 from django.test import SimpleTestCase
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from showcase.forms import ItemModelForm
 
 
-class TestForms(SimpleTestCase):
+def create_test_image(width, height):
+    file = BytesIO()
+    image = Image.new('RGBA', size=(width, height), color=(155, 0, 0))
+    image.save(file, 'png')
+    file.name = 'test.png'
+    file.seek(0)
+    return InMemoryUploadedFile(
+        file=file,
+        field_name='image',
+        name='test.png',
+        content_type='image/png',
+        size=45,
+        charset='utf-8'
+    )
 
+
+class TestForms(SimpleTestCase):
     def test_item_model_form_no_data(self):
         form = ItemModelForm(data={})
         self.assertFalse(form.is_valid())
         self.assertEquals(len(form.errors), 4)
 
-    #def test_item_model_form_valid_data(self):
-    #    form = ItemModelForm(
-    #        data={
-    #            'title': 'Title',
-    #            'description': 'x' * 100,
-    #            'price': 100,
-    #            'image': None
-    #        }
-    #    )
-    #    self.assertTrue(form.is_valid())
-    #    print(form.errors.as_text)
+    def test_item_model_form_clean_image_method_valid_format(self):
+        data = {
+            'title': 'test_title`',
+            'description': 'test_description' * 10,
+            'price': 500
+        }
+        file_data = {
+            'image': create_test_image(600, 600)
+        }
+        form = ItemModelForm(data=data, files=file_data)
+        self.assertTrue(form.is_valid())
+
+    def test_item_model_form_clean_image_method_invalid_format(self):
+        data = {
+            'title': 'test_title`',
+            'description': 'test_description' * 10,
+            'price': 500
+        }
+        file_data = {
+            'image': create_test_image(600, 601)
+        }
+        form = ItemModelForm(data=data, files=file_data)
+        self.assertFalse(form.is_valid())
