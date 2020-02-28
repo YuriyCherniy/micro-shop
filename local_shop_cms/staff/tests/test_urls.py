59c81a02decad28d2803641d02b2a4188bf6854a
Yuriy Cherniy <amidobox@gmail.com>
2020-02-29 00:49:19 +0300

    tests: added url tests for staff app

---
+++ local_shop_cms/staff/tests/test_urls.py
@@ -0,0 +1,12 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from staff.views import UserProfileUpdate
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_profile_url_resolved(self):
+        url = reverse('profile_url', args=[1])
+        self.assertEquals(resolve(url).func.view_class, UserProfileUpdate)
+        print(resolve(url))
