dfb675ef877e10fc1c935efc8a980ee74bf0e443
Yuriy Cherniy <amidobox@gmail.com>
2020-02-28 23:12:13 +0300

    chore: minor changes

--- local_shop_cms/mainpage/urls.py
+++ local_shop_cms/mainpage/urls.py
@@ -10,7 +10,7 @@ from .views import (
 
 urlpatterns = [
     path('', MainPageEditorList.as_view(), name='main_page_editor_url'),
-    path('<int:pk>/delete', MainPageEditorDelete.as_view(), name='delete_item_from_main_page_url'),
-    path('<int:pk>/update', MainPageEditorUpdate.as_view(), name='update_item_on_main_page_url'),
+    path('<int:pk>/delete/', MainPageEditorDelete.as_view(), name='delete_item_from_main_page_url'),
+    path('<int:pk>/update/', MainPageEditorUpdate.as_view(), name='update_item_on_main_page_url'),
     path('add-item/', MainPageEditorCreate.as_view(), name='add_item_to_main_page_url')
 ]
