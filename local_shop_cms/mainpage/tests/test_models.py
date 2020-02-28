0de196265b22294bd774496685700a343d4ac066
Yuriy Cherniy <amidobox@gmail.com>
2020-02-29 00:11:53 +0300

    tests: added model tests for mainpage app

---
+++ local_shop_cms/mainpage/tests/test_models.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from showcase.models import Item
+from mainpage.models import ItemOnMainPage
+
+
+class TestItemOnMainPageModel(TestCase):
+
+    def setUp(self):
+        self.item = Item.objects.create(
+            title='test title',
+            description='test description',
+            price=100,
+            image='1.jpg'
+        )
+        self.on_main_page = ItemOnMainPage.objects.create(
+            item_on_main_page_id=1,
+            position=1
+        )
+
+    def test__str__method(self):
+        self.assertEquals(
+            repr(self.on_main_page), '<ItemOnMainPage: Test title>'
+        )
+
+    def test_get_absolute_url_method(self):
+        self.assertEquals(
+            self.on_main_page.get_absolute_url(), '/items/item-1/'
+        )
