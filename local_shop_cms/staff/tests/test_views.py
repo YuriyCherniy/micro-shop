f7cb9c19600b6f21f81a068f6091998581cbab50
Yuriy Cherniy <amidobox@gmail.com>
2020-02-29 11:35:27 +0300

    tests: added some tests

--- local_shop_cms/staff/tests/test_views.py
+++ local_shop_cms/staff/tests/test_views.py
@@ -1,11 +1,29 @@
-from django.test import SimpleTestCase, Client
+from django.test import TestCase, Client
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+from staff.models import UserProfile
 
 
-class TestViews(SimpleTestCase):
+class TestViews(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_superuser(
+            username='test', email='test@mail.ru', password='0000'
+        )
+        UserProfile.objects.create(user_id=1)
+
     def setUp(self):
         self.c = Client()
 
-    def test_user_profile_detail_view_status_code_403(self):
+    def test_user_profile_update_view_status_code_403(self):
         response = self.c.get(reverse('profile_url', args=[1]))
         self.assertEquals(response.status_code, 403)
+
+    def test_user_profile_update_view_status_code_200(self):
+        self.c.post(
+            '/admin/login/', {'username': 'test', 'password': '0000'}
+        )
+        response = self.c.get(reverse('profile_url', args=[1]))
+        self.assertEquals(response.status_code, 200)
