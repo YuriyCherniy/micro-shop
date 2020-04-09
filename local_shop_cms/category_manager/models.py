18de4394b3a1ecc21f397a1f63bf7a4343c0b3c8
Yuriy Cherniy <amidobox@gmail.com>
2020-04-09 13:32:17 +0300

    fix: register 'Category' model for django-admin-panel

--- local_shop_cms/category_manager/models.py
+++ local_shop_cms/category_manager/models.py
@@ -3,6 +3,9 @@ from django.urls import reverse
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'категории'
+
     title = models.CharField(
         max_length=20,
         unique=True,
