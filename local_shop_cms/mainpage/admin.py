846911d74de029d23b5bf4fcb2f0ae0060a9c5ca
Yuriy Cherniy <amidobox@gmail.com>
2020-04-22 20:46:00 +0300

    fix: delete managing of models from admin interface

--- local_shop_cms/mainpage/admin.py
+++ local_shop_cms/mainpage/admin.py
@@ -1,5 +1,6 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-from .models import ItemOnMainPage
+# from .models import ItemOnMainPage
 
-admin.site.register(ItemOnMainPage)
+
+# admin.site.register(ItemOnMainPage)
--- local_shop_cms/showcase/admin.py
+++ local_shop_cms/showcase/admin.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-from .models import Item
+# from .models import Item
 
 
-admin.site.register(Item)
+# admin.site.register(Item)
