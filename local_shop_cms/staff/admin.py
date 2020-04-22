e911767e26faa5bda808cc43a2067994db5c01ca
Yuriy Cherniy <amidobox@gmail.com>
2020-04-22 20:59:05 +0300

    fix: unregister Group model in admin panel

--- local_shop_cms/staff/admin.py
+++ local_shop_cms/staff/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from .models import UserProfile
 
 
 admin.site.register(UserProfile)
+
+admin.site.unregister(Group)
