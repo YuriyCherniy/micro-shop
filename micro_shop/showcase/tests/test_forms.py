84adcae179ce41a91fb65562787c2b8e1a091462
Yuriy Cherniy <amidobox@gmail.com>
2020-06-06 13:59:17 +0300

    added some tests

--- micro_shop/showcase/tests/test_forms.py
+++ micro_shop/showcase/tests/test_forms.py
@@ -8,7 +8,7 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 from showcase.forms import ItemModelForm
 
 
-def create_test_image(width, height):
+def create_test_image(width, height, size):
     file = BytesIO()
     image = Image.new('RGBA', size=(width, height), color=(155, 0, 0))
     image.save(file, 'png')
@@ -19,7 +19,7 @@ def create_test_image(width, height):
         field_name='image',
         name='test.png',
         content_type='image/png',
-        size=45,
+        size=size,
         charset='utf-8'
     )
 
@@ -36,17 +36,24 @@ class TestForms(SimpleTestCase):
         form = ItemModelForm(data={})
         self.assertEqual(len(form.errors), 5)
 
-    def test_item_model_form_clean_image_format_1x1_valid_data(self):
+    def test_item_model_form_clean_image_valid_data(self):
         file_data = {
-            'image': create_test_image(600, 600)
+            'image': create_test_image(600, 600, 1000000)
         }
         form = ItemModelForm(data=self.data, files=file_data,)
-        form.is_valid()
         self.assertTrue(form.is_valid())
 
     def test_item_model_form_clean_image_format_1x1_invalid_data(self):
         file_data = {
-            'image': create_test_image(600, 601)
+            'image': create_test_image(600, 601, 1000000)
         }
         form = ItemModelForm(data=self.data, files=file_data)
+        self.assertFalse(form.is_valid())
+
+    def test_item_model_form_clean_image_size_bigger_then_1000000(self):
+        file_data = {
+            'image': create_test_image(600, 600, 1000001)
+        }
+        form = ItemModelForm(data=self.data, files=file_data,)
+        form.is_valid()
         self.assertFalse(form.is_valid())
